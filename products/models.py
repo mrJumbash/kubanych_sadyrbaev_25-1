5947471fa9993cd0092e3c2ab9f99069b8fc7003
mrJumbash <jumbash00@gmail.com>
2023-02-27 16:02:26 +0600

    .

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Hashtag(models.Model):
     title = models.CharField(max_length=55)
@@ -12,17 +11,18 @@ class Hashtag(models.Model):
 class Product(models.Model):
     image = models.ImageField(blank=True, null=True)
     title = models.CharField(max_length=255)
-    description = models.TextField()
-    rating = models.FloatField(default=0.0)
-    created_date = models.DateField(auto_now_add=True)
-    modified_date = models.DateField(auto_now=True)
+    description = models.TextField(null=True)
+    quantity = models.IntegerField(null=True)
     price = models.FloatField(default=0.0)
     hashtags = models.ManyToManyField(Hashtag)
+    created_date = models.DateField(auto_now_add=True)
+    modified_date = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
 
-class Comment(models.Model):
+
+class Review(models.Model):
     text = models.CharField(max_length=255)
-    post = models.ForeignKey(Product, on_delete=models.CASCADE)
-    created_date = models.DateField(auto_now_add=True, null=True)
+    rate = models.FloatField(default=0.0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
